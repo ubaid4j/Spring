4c891f2d59e85e36b4123872a6207b07019cd6df
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-07-24 17:58:33 +0500

    @PathVariable add

--- RestFul1/readme.md
+++ RestFul1/readme.md
@@ -4,3 +4,9 @@
 	<li>The only change is to replace <strong>@Controller</strong> with <strong>@RestController</strong></li>
 	<li>Add dependency of jackson data binding so that spring will automatically convert POJO to json data</li>
 </ol>
+
+<ol>
+<h2>@PathVariable</h2>
+	<li>In @GetMapping("/student/{id})" and</li>
+	<li>and in the parameters @PathVariable type id</li>
+</ol>
--- RestFul1/src/main/java/com/ubaid/app/controller/StudentController.java
+++ RestFul1/src/main/java/com/ubaid/app/controller/StudentController.java
@@ -3,7 +3,10 @@ package com.ubaid.app.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,29 @@ import com.ubaid.app.entity.Student;
 @RequestMapping("/api")
 public class StudentController
 {
-	@GetMapping("/students")
-	public List<Student> getStudents()
+	
+	private List<Student> students;
+	
+	@PostConstruct
+	public void load()
 	{
-		List<Student> students = new ArrayList<Student>();
-		
+		students = new ArrayList<Student>();
 		students.add(new Student("Ubaid", "ur Rehman"));
 		students.add(new Student("Attiq", "Rehman"));
 		students.add(new Student("Ahsan", "Farooq"));
 		students.add(new Student("Kashif", "Nazir"));
-		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents()
+	{
 		return students;
+	}
+	
+	
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable int id)
+	{
+		return students.get(id);
 	}
 }
