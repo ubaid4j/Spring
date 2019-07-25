ef65456571780ee9395439346b0e4f3612ee7966
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-07-25 19:37:08 +0500

    get, add, update, delete completed

--- RestFul2_CRUD/src/main/java/com/ubaid/app/rest/CustomerRestController.java
+++ RestFul2_CRUD/src/main/java/com/ubaid/app/rest/CustomerRestController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +25,22 @@ public class CustomerRestController
 	@Qualifier("customerServiceImp")
 	CustomerServiceI custSer;
 	
+	/**
+	 * this method returns all customers
+	 * @return all customers
+	 */
 	@GetMapping("/customers")
 	public List<Customer> getCustomers()
 	{
 		return custSer.getCustomers();
 	}
 	
+	
+	/**
+	 * this method return a customer
+	 * @param customerId
+	 * @return a customer
+	 */
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId)
 	{
@@ -36,9 +50,42 @@ public class CustomerRestController
 		return custSer.getCustomer(customerId);
 	}
 	
-	public Customer addCustomer()
+	/**
+	 * this method add a customer
+	 * @param customer
+	 * @return 
+	 */
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer)
 	{
-		
-		return null;
+		customer.setId(0);
+		return custSer.saveCustomer(customer);
+	}
+	
+	/**
+	 * this method update a customer
+	 * @param customer
+	 * @return
+	 */
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer)
+	{
+		return custSer.saveCustomer(customer);
+	}
+	
+	
+	/**
+	 * this method delete a customer
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/customers/{id}")
+	public String deleteCustomer(@PathVariable int id)
+	{
+		Customer customer = custSer.getCustomer(id);
+		if(customer == null) 
+			throw new CustomerNotFoundException("The customer of id [ " + id + " ] is not found");
+		custSer.deleteCustomer(id);
+		return "Customer deleted ~ id [ " + id + " ]";
 	}
 }
